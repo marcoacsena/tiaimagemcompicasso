9a790f34c464cdfd96b6e40231f2b075bd9777fa
marco sena <marcoacsena@gmail.com>
2020-07-14 23:07:06 -0300

    Initial commit

---
+++ app/src/main/java/com/example/imageviewwithweb/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.imageviewwithweb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView ivImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivImage = findViewById(R.id.ivImage);
+        Picasso.get().load("https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg")
+                .resize(200, 100)
+                .centerCrop()
+                .into(ivImage);
+
+    }
+}
